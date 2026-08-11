2f82153d3475f595a72cdaa8d3a54003b117c79e
bdredenbach <bdredenbach@gmail.com>
2026-08-11 17:42:24 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 
    v7: fluid playback progress bar build I3.1.
    The page itself remains network-first so new versions are picked up promptly. */
-const CACHE = 'comic-scorer-v9';
+const CACHE = 'comic-scorer-v10';
 
 const SHELL = [
   './',
